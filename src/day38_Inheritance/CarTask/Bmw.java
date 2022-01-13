6fe4fa53caa380549fa74d49532b050c424b51d6
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-01-13 10:02:30 -0600

    changes in tesla and mmw class

--- src/day38_Inheritance/CarTask/Bmw.java
+++ src/day38_Inheritance/CarTask/Bmw.java
@@ -4,5 +4,8 @@ public class Bmw extends Car{
     public Bmw( String model, int year, double price, String color, int miles) {
         super("BMW", model, year, price, color, miles);
     }
+public void breaksDown(){
+    System.out.println(brand+ model+ "is breaking down a lot");
+}
 
 }
--- src/day38_Inheritance/CarTask/Tesla.java
+++ src/day38_Inheritance/CarTask/Tesla.java
@@ -5,5 +5,7 @@ public class Tesla extends Car{
     public Tesla( String model, int year, double price, String color, int miles) {
         super("Tesla", model, year, price, color, miles);
     }
-
+public void autopilot(){
+    System.out.println(brand+model+"has autopilot function");
+}
 }
