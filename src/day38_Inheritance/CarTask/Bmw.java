2cbd4e69a43e5b2f3ac218a7aea68bd542b70453
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-01-13 09:57:58 -0600

    bmw class

---
+++ src/day38_Inheritance/CarTask/Bmw.java
@@ -0,0 +1,8 @@
+package day38_Inheritance.CarTask;
+
+public class Bmw extends Car{
+    public Bmw( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+}
--- src/day38_Inheritance/CarTask/Toyota.java
+++ src/day38_Inheritance/CarTask/Toyota.java
@@ -3,10 +3,10 @@ package day38_Inheritance.CarTask;
 public class Toyota extends Car {
 
 
-    public Toyota(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Toyota(String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
     }
     public void isReliable(){
-        System.out.println("Toyota"+ model+ " is very reliable");
+        System.out.println(brand+ model+ " is very reliable");
     }
 }
