22f9c8a275c0150c10dca6a99224be8a977a6e17
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-01-14 10:32:38 -0600

    day39_changes

--- src/day39/Shapes/Square.java
+++ src/day39/Shapes/Square.java
@@ -1,7 +1,29 @@
 package day39.Shapes;
 
-public class Square{
+public class Square extends Shapes{
+ private double side;
 
+    public Square(String name) {
+        super(name);
     }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side<=0){
+            System.err.println("Invalid side size"+side);
+            System.exit(0);
+        }
+        this.side = side;
+    }
+
+    public Square( double side) {
+        super("Square");
+        setSide(side);
+    }
+
+}
 
 
